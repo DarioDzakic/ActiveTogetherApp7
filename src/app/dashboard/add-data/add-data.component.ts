4f4873969555c481c856a5eda12abab0cfed0a00
Zoxh <dario.dzakic@gmail.com>
2024-12-18 20:03:34 +0100

    temp1

--- src/app/dashboard/add-data/add-data.component.ts
+++ src/app/dashboard/add-data/add-data.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
-import { Validators, FormBuilder } from '@angular/forms';
+import { Validators, FormBuilder, FormsModule } from '@angular/forms';
 import { StoreService } from '../../shared/store.service';
 import { BackendService } from '../../shared/backend.service';
 import {MatDatepickerModule} from '@angular/material/datepicker';
@@ -9,10 +9,13 @@ import {MatError, MatFormFieldModule} from '@angular/material/form-field';
 import {MatNativeDateModule} from '@angular/material/core';
 import {MatSelectModule} from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+
 @Component({
   selector: 'app-add-data',
   standalone: true,  // standalone-Komponente
-  imports: [SharedModule, MatInputModule, MatFormFieldModule, MatError,
+  imports: [SharedModule, MatInputModule, MatFormFieldModule, MatError, MatCheckboxModule, FormsModule,
      MatDatepickerModule, MatNativeDateModule, MatSelectModule, MatButtonModule],  // Import der benötigten Module
   templateUrl: './add-data.component.html',
   styleUrls: ['./add-data.component.css'],
@@ -22,22 +25,25 @@ export class AddDataComponent implements OnInit {
   constructor(private formbuilder: FormBuilder, public storeService: StoreService, private backendService: BackendService) {
   }
   public registrationForm: any;
+  public newsletter: boolean = false;
+
+
 
   ngOnInit(): void {
     this.registrationForm = this.formbuilder.group({
-      name: ['', [Validators.required]],
+      name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(60)]],
       courseId: ['', Validators.required],
-      birthdate: [null, Validators.required]
+      birthdate: [null, Validators.required],
+      email: [null, [Validators.email]]
     })
+    
   }
+
 
   onSubmit() {
     if(this.registrationForm.valid) {
       this.backendService.addRegistration(this.registrationForm.value, this.storeService.currentPage);
     }
-  }
-  show() {
-    document.getElementById("")
   }
 
 }
